82155370e79113740bdb4921888244634d4d2232
Marc Vertido <marc.vertido@gmail.com>
2021-10-07 18:17:00 -0400

    Initial upgrades and removals
    - No MvvmCross
    - Target .NET Standard 2.0 for main projects
    - Remove Newtonsoft.JSON (going to use System.Net.Json instead)
    - Latest versions of things

--- MvvmCross/IntegrationTests.Touch-MvvmCross/Bootstrap/SqlitePluginBootstrap.cs
+++
@@ -1,8 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace IntegrationTests.TouchMvvmCross.Bootstrap
-{
-    public class SqlitePluginBootstrap
-        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.Sqlite.PluginLoader, MvvmCross.Plugins.Sqlite.iOS.Plugin>
-	{}
-}
